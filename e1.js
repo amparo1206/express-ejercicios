a70245124d3eb5c060c48bd9875cc171b9b1cf05
amparo1206 <amparo.frontend@gmail.com>
2022-01-17 12:21:44 +0100

    aprendiendo express

---
+++ e1.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.use(express.json())
+
+app.listen(4050, () => {
+    console.log("el servidor está en el puerto 4050")
+})
+app.listen(4050, (err) => {
+    if (err) {
+        console.log(`Servidor levantado en el puerto ${puerto}`)
+    } else {
+        console.log(`Servidor no se ha levantado en el puerto ${puerto}`)
+    }
+})
+app.get('/',(req,res)=>{
+    res.send('Bienvenidos a Node.js Server Side')
+})
