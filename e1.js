4a6a13ea514826bfc0a647c4abdd918e0fbca284
amparo1206 <amparo.frontend@gmail.com>
2022-01-17 12:40:49 +0100

    añadiendo ej2

--- e1.js
+++ e1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 
-app.listen(4050, () => {
+/* app.listen(4050, () => {
     console.log("el servidor está en el puerto 4050")
 })
 app.listen(4050, (err) => {
@@ -11,7 +11,7 @@ app.listen(4050, (err) => {
     } else {
         console.log(`Servidor no se ha levantado en el puerto ${puerto}`)
     }
-})
+}) */
 app.get('/',(req,res)=>{
     res.send('Bienvenidos a Node.js Server Side')
 })
