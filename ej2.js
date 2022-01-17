cb2d29863053fe0dac9f4dd6301181e09001c6a6
amparo1206 <amparo.frontend@gmail.com>
2022-01-17 15:28:33 +0100

    ejercicio 2 terminado

--- ej2.js
+++ ej2.js
@@ -3,16 +3,44 @@ const path = require("path")
 const app = express();
 
 app.use(express.json())
-
-/* app.get('/',(req,res)=>{
-    res.send('Welcome, ¡hola Mundo!')
-}) */
-
+/* 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'))
-}) 
+})  */
 
-app.use(express.static(path.join(__dirname, 'public')));
+/* app.use(express.static(path.join(__dirname, 'public'))); */
+
+app.get('/products',(req,res)=>{
+    res.send('listado de productos')
+})
+
+app.post('/products',(req,res)=>{
+    res.send('crear productos')
+})
+
+app.put('/products',(req,res)=>{
+    res.send('actualizar un producto')
+})
+
+app.delete('/products',(req,res)=>{
+    res.send('borrar un producto')
+})
+
+app.get('/usuarios',(req,res)=>{
+    res.send('listado de usuarios')
+})
+
+app.post('/usuarios', (req, res) => {
+    res.send('crear usuarios')
+})
+
+app.put('/usuarios', (req, res) => {
+    res.send('actualizar un usuario')
+})
+
+app.delete('/usuarios', (req, res) => {
+    res.send('borrar un usuario')
+})
 
 app.listen(4050, () => {
     console.log("el servidor está en el puerto 4050")
